26a9a2f64c2e80a76d1acf081c4d68837f48e59c
Thalles Alexsander <thallesalexsandermachado@gmail.com>
2026-01-02 17:08:26 -0300

    mudei algumas coisas de lugar

--- Tutoriais/Tutoriais/VS-code/site-tutorial/tutorial-java/Ola-mundo.java/src/OlaMundo.java
+++ Tutoriais/Tutoriais/VS-code/site-tutorial/tutorial-java/Ola-mundo.java/src/OlaMundo.java
@@ -1,6 +1,9 @@
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Ola, Mundo");
+        int a = 2;
+        System.out.println(a);
+
     }
     
 }
